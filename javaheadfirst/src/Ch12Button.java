c42aad0b89c127436b9aff96d62dcab41c10bba8
bro <bro@bro.bro>
2017-12-20 15:29:50 +0800

    add click event to Ch12Button exercise

--- javaheadfirst/src/Ch12Button.java
+++ javaheadfirst/src/Ch12Button.java
@@ -1,13 +1,27 @@
 import javax.swing.*;
 import java.awt.event.*;
 
-public class Ch12Button {
-	public static void main(String[] args) {
-		JFrame frame = new JFrame("Booyah");
-		JButton button = new JButton("CLICK");
+public class Ch12Button implements ActionListener {
+	private JFrame frame;
+	private JButton button;
+	
+	public void go() {
+		frame = new JFrame("Booyah");
+		button = new JButton("CLICK");
+		button.addActionListener(this);
 		frame.getContentPane().add(button);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setSize(640, 480);
-		frame.setVisible(true);
+		frame.setVisible(true);	
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		button.setText("CLICKED");
+		frame.setSize(320, 240);
+	}
+	
+	public static void main(String[] args) {
+		Ch12Button bro = new Ch12Button();
+		bro.go();
 	}
 }
