495613e2e38cfb283bd7e249e1b9683ffc91ac9f
bro <bro@bro.bro>
2018-01-11 17:27:54 +0800

    added events to send button and text field in SimpleChatClient.java

--- javaheadfirst/src/SimpleChatClient.java
+++ javaheadfirst/src/SimpleChatClient.java
@@ -4,38 +4,62 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class SimpleChatClient {
-	JFrame frame;
+	private JFrame frame;
+	private JTextArea chatDisplay;
+	private JTextField sendText;
 	
-	public void gui() {
-		JTextArea chatDisplay;
+	public void go() {
+		this.gui();
+		this.network();
+	}
+	
+	private void gui() {
 		JScrollPane chatDisplayScroll;
 		JPanel sendPanel;
-		JTextField sendText;
 		JButton sendButton;
+		SendButtonListener sendButtonListener = new SendButtonListener();
 		frame = new JFrame("SimpleChatClient");
 		chatDisplay = new JTextArea();
-		chatDisplay.setPreferredSize(new Dimension(400, 300));
 		chatDisplay.setEditable(false);
 		chatDisplay.setLineWrap(true);
 		chatDisplay.setWrapStyleWord(true);
 		chatDisplayScroll = new JScrollPane(chatDisplay);
+		chatDisplayScroll.setPreferredSize(new Dimension(400, 300));
 		chatDisplayScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		chatDisplayScroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		frame.add(chatDisplayScroll, BorderLayout.CENTER);
 		sendPanel = new JPanel();
 		sendPanel.setLayout(new BorderLayout());
-		sendText = new JTextField(30);
+		sendText = new JTextField();
+		sendText.addActionListener(sendButtonListener);
 		sendPanel.add(sendText, BorderLayout.CENTER);
 		sendButton = new JButton("SEND");
+		sendButton.addActionListener(sendButtonListener);
 		sendPanel.add(sendButton, BorderLayout.EAST);
 		frame.add(sendPanel, BorderLayout.SOUTH);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
 		frame.setVisible(true);
+		while (!sendText.hasFocus())
+			sendText.requestFocus();
+	}
+	
+	private void network() {
+		
 	}
 	
 	public static void main(String[] args) {
 		SimpleChatClient client = new SimpleChatClient();
-		client.gui();
+		client.go();
+	}
+	
+	private class SendButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			sendText.requestFocus();
+			if (sendText.getText().equals(""))
+				return;
+			chatDisplay.append("> " + sendText.getText() + "\n");
+			sendText.setText("");
+		}
 	}
 }
