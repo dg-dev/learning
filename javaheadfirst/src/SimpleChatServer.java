efce85a8e19a5316805c7f2eb74f36251cdf227f
bro <bro@bro.bro>
2018-01-13 22:12:14 +0800

    finished SimpleChatServer.java

--- javaheadfirst/src/SimpleChatServer.java
+++ javaheadfirst/src/SimpleChatServer.java
@@ -1,7 +1,24 @@
 import java.net.*;
 import java.io.*;
+import java.util.HashSet;
 
 public class SimpleChatServer {
+	private HashSet<Socket> connectedClients;
+	
+	public SimpleChatServer() {
+		connectedClients = new HashSet<Socket>();
+	}
+	
+	private void addClient(Socket sock) {
+		connectedClients.add(sock);
+		System.out.println("connected clients: " + connectedClients);
+	}
+	
+	private void removeClient(Socket sock) {
+		connectedClients.remove(sock);
+		System.out.println("connected clients: " + connectedClients);
+	}
+	
 	public void go() {
 		try {
 			Socket cSock = null;
@@ -28,9 +45,10 @@ public class SimpleChatServer {
 		private Socket sock;
 		
 		public ChatReaderRunnable(Socket s) {
-			sock = s;
+			this.sock = s;
 			try {
 				reader = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+				addClient(sock);
 				System.out.println("connected " + sock.toString());
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -42,12 +60,31 @@ public class SimpleChatServer {
 			try {
 				while ((line = reader.readLine()) != null) {
 					System.out.println("> " + line);
+					broadcast(line);
 				}
 				reader.close();
+			} catch (SocketException e) {
+				System.out.println(e.getMessage());
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+			removeClient(sock);
 			System.out.println("disconnected " + sock.toString());
+		}
+		
+		public void broadcast(String message) {
+			for (Socket cSock : connectedClients) {
+				if (this.sock == cSock)
+					continue;
+				try {
+					PrintWriter writer = new PrintWriter(cSock.getOutputStream());
+					writer.println(message);
+					writer.flush();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+			}
 		}
 	}
 }
