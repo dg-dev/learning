3081671d21a6bbb5e8466016cc3ad078bd26d88d
bro <bro@bro.bro>
2018-01-20 23:07:21 +0800

    Added Media and MediaType to wizardhat.

---
+++ wizardhat/src/dg/projects/wizardhat/Media.java
@@ -0,0 +1,25 @@
+package dg.projects.wizardhat;
+
+public class Media {
+	private MediaType type;
+	private String title;
+	private String link;
+	
+	public Media(MediaType type, String title, String link) {
+		this.type = type;
+		this.title = title;
+		this.link = link;
+	}
+	
+	public MediaType getType() {
+		return type;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getLink() {
+		return link;
+	}
+}
---
+++ wizardhat/src/dg/projects/wizardhat/MediaType.java
@@ -0,0 +1,5 @@
+package dg.projects.wizardhat;
+
+public enum MediaType {
+	TORRENT, MAGNET, NZB, HTTP, FTP;
+}
